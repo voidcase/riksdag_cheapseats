7b53fc86dde1689add99835646cc29c2a20eb8e4
Isak Lindhé <isak.e.lindhe@gmail.com>
2018-04-14 12:38:50 +0200

    populating in tests

--- backend/test_server.py
+++ backend/test_server.py
@@ -7,12 +7,17 @@ import json
 def tester():
     return app.test_client()
 
-def test_topics(tester):
+@pytest.fixture
+def pop_db():
+    from models import reset_and_populate
+    reset_and_populate()
+
+def test_topics(tester, pop_db):
     res = tester.get('/topics', content_type='application/json')
     assert res.status_code == 200
 
 
-def test_topic(tester):
+def test_topic(tester, pop_db):
     res = tester.get('/topic/1', content_type='application/json')
     assert res.status_code == 200
     assert "error" not in json.loads(res.data)
