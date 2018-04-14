b409bcf7c5c52555ede78c4dd21e40c33beb613b
Karl-Oskar Rikås <fuuunrep@gmail.com>
2018-04-14 11:40:53 +0200

    updated web scrape seeder

--- backend/models.py
+++ backend/models.py
@@ -4,11 +4,22 @@ from os import environ
 import psycopg2
 
 db = connect(environ.get('DATABASE_URL') or 'sqlite:///test.db')
+
 class BaseModel(pw.Model):
     class Meta:
         database = db
 
 class Document(BaseModel):
     # automagic id integer field
-    title = pw.CharField(max_length=140)
     body = pw.TextField()
+
+class Debate(BaseModel):
+    id = pw.CharField(primary_key=True)
+    title = pw.CharField(max_length=140)
+    summary = pw.TextField()
+
+class Speech(Document):
+    person = pw.CharField(max_length=140)
+    debate_id = pw.ForeignKeyField(Debate)
+    nr = pw.IntegerField()
+    party = pw.CharField(max_length=4)
